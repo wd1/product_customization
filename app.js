c61ab06debe44b3df2e2558bcfcf57b331d3ad39
wd1 <akmksi211@gmail.com>
2017-09-26 22:59:34 -0400

    Facebook Bot

--- app.js
+++ app.js
@@ -55,4 +55,19 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//for facebook chatbot
+
+var request = require('request')
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/', function(req,res) {
+  if(req.query['hub.verify_token'] === 'blondiebytes') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send("Wrong token")
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Running: port")
+})
 module.exports = app;
