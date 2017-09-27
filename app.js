99884fa857f02b1e2ed28bdddfa8e20762813c0a
wd1 <akmksi211@gmail.com>
2017-09-27 10:37:00 -0400

    Remove facebook

--- app.js
+++ app.js
@@ -1,79 +1,58 @@
 var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
 
-// var index = require('./routes/index');
-// var about = require('./routes/about');
-// var contact = require('./routes/contact');
+var index = require('./routes/index');
+var about = require('./routes/about');
+var contact = require('./routes/contact');
 var request = require('request')
 
 var app = express();
 
-// var cons = require('consolidate');
+var cons = require('consolidate');
 
-// // view engine setup
-// app.engine('html', cons.swig)
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// view engine setup
 // app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'html');
+// app.set('view engine', 'jade');
 
-// // view engine setup
-// // app.set('views', path.join(__dirname, 'views'));
-// // app.set('view engine', 'jade');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({limit: '5mb'}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json({limit: '5mb'}));
-// app.use(bodyParser.urlencoded({limit: '5mb'}));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+app.use('/about', about);
+app.use('/contact', contact);
 
-// app.use('/', index);
-// app.use('/about', about);
-// app.use('/contact', contact);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-//for facebook chatbot
-
-
-app.set('port', (process.env.PORT || 5000))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', function(req, res) {
-  res.send("Hi I am a chatbot")
-})
-
-app.get('/webhook/', function(req,res) {
-  if(req.query['hub.verify_token'] === 'blondiebytes') {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send("Wrong token")
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Running: port")
-})
-module.exports = app;
