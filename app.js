77d4b2569e11364c0fab571ec92432c102246385
wd1 <akmksi211@gmail.com>
2017-09-27 10:27:10 -0400

    Change to Facebook

--- app.js
+++ app.js
@@ -13,48 +13,48 @@ var request = require('request')
 
 var app = express();
 
-var cons = require('consolidate');
+// var cons = require('consolidate');
 
-// view engine setup
-app.engine('html', cons.swig)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-
-// view engine setup
+// // view engine setup
+// app.engine('html', cons.swig)
 // app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+// app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({limit: '5mb'}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// // view engine setup
+// // app.set('views', path.join(__dirname, 'views'));
+// // app.set('view engine', 'jade');
 
-app.use('/', index);
-app.use('/about', about);
-app.use('/contact', contact);
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// // app.use(bodyParser.json());
+// // app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json({limit: '5mb'}));
+// app.use(bodyParser.urlencoded({limit: '5mb'}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use('/', index);
+// app.use('/about', about);
+// app.use('/contact', contact);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 //for facebook chatbot
 
@@ -68,7 +68,7 @@ app.get('/webhook/', function(req,res) {
   res.send("Wrong token")
 })
 
-// app.listen(app.get('port'), function() {
-//   console.log("Running: port")
-// })
+app.listen(app.get('port'), function() {
+  console.log("Running: port")
+})
 module.exports = app;
